33a18c05337aef8448cf3fd6918ae53a4fbed2f7
Asier <blazgar@gmail.com>
2017-02-23 11:09:20 +0100

    falla facade, registro correcto

--- src/gui/RegistroGUI.java
+++ src/gui/RegistroGUI.java
@@ -21,17 +21,14 @@ import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JPasswordField;
 import domain.*;
-import exceptions.*;
+import exceptions.OverlappingUsersExists;
 
 public class RegistroGUI extends JFrame {
-	
-	
+
 	private String nombre;
 	private String usuario;
 	private String pass;
 	private String cuenta;
-	//private boolean tipoOwner;
-	private boolean tipoCliente;
 	private final ButtonGroup TipoUsuario = new ButtonGroup();
 	private JRadioButton rdbtnCliente;
 	private JRadioButton rdbtnOwner;
@@ -41,6 +38,7 @@ public class RegistroGUI extends JFrame {
 	private JTextField textUsuario;
 	private JTextField textCuenta;
 	
+	private JButton btnRegistro;
 	private JPasswordField passwordField;
 
 	/**
@@ -69,125 +67,189 @@ public class RegistroGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		textNombre = new JTextField();
 		textNombre.setBounds(179, 71, 156, 22);
 		contentPane.add(textNombre);
 		textNombre.setColumns(10);
-		
+
 		JLabel lblNombre = new JLabel("Nombre:");
 		lblNombre.setBounds(54, 74, 56, 16);
 		contentPane.add(lblNombre);
-		
+
 		textUsuario = new JTextField();
 		textUsuario.setBounds(179, 106, 156, 22);
 		contentPane.add(textUsuario);
 		textUsuario.setColumns(10);
-		
+
 		JLabel lblRegistro = new JLabel("Registro");
 		lblRegistro.setFont(new Font("Tahoma", Font.PLAIN, 19));
 		lblRegistro.setBounds(213, 13, 147, 25);
 		contentPane.add(lblRegistro);
-		
+
 		JLabel lblNombreDeUsuario = new JLabel("Nombre de Usuario:");
 		lblNombreDeUsuario.setBounds(54, 106, 131, 22);
 		contentPane.add(lblNombreDeUsuario);
-		
+
 		JLabel lblCuentaBancaria = new JLabel("Cuenta Bancaria:");
 		lblCuentaBancaria.setBounds(54, 182, 113, 16);
 		contentPane.add(lblCuentaBancaria);
-		
+
 		textCuenta = new JTextField();
 		textCuenta.setBounds(179, 179, 156, 22);
 		contentPane.add(textCuenta);
 		textCuenta.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Registrarse");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				try{
-				
-					ApplicationFacadeInterfaceWS facade = MainGUI.getBusinessLogic();
-					//tipoOwner= rdbtnOwner.isSelected();
-					tipoCliente= rdbtnCliente.isSelected();
-					nombre= textNombre.getText();
-					usuario= textUsuario.getText();
-					cuenta= textCuenta.getText();
-					if(!ComprobarCamposVacios()){
-						
-						if(tipoCliente){
-							Users u = facade.crearCliente(nombre, usuario, pass, cuenta);
-							System.out.println(u.toString() + "registrado como Cliente;");
-						}
-						
-					}
-				
-				} catch (Exception e) {
-					//showErrorDialog();
-				//} catch (Exception ex) {
-					//ex.printStackTrace();
-				}
-				
-				
-			}
-		});
-		btnNewButton.setBounds(205, 253, 107, 25);
-		contentPane.add(btnNewButton);
-		
+
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFrame vuelta= new MainGUI();
+				JFrame vuelta = new MainGUI();
 				vuelta.setVisible(true);
 				setVisible(false);
 			}
 		});
 		btnVolver.setBounds(36, 305, 97, 25);
 		contentPane.add(btnVolver);
-		
+
 		JButton btnEntrarEnEl = new JButton("Entrar en el Sistema");
 		btnEntrarEnEl.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFrame login= new LoginGUI();
+				JFrame login = new LoginGUI();
 				login.setVisible(true);
 				setVisible(false);
-				
+
 			}
 		});
 		btnEntrarEnEl.setBounds(179, 305, 156, 25);
 		contentPane.add(btnEntrarEnEl);
-		
+
 		rdbtnCliente = new JRadioButton("Cliente");
 		TipoUsuario.add(rdbtnCliente);
 		rdbtnCliente.setBounds(166, 214, 67, 25);
 		contentPane.add(rdbtnCliente);
-		
+
 		rdbtnOwner = new JRadioButton("Owner");
 		TipoUsuario.add(rdbtnOwner);
 		rdbtnOwner.setBounds(268, 214, 67, 25);
 		contentPane.add(rdbtnOwner);
-		
+
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
 		lblContrasea.setBounds(54, 147, 76, 16);
 		contentPane.add(lblContrasea);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(179, 144, 156, 22);
 		contentPane.add(passwordField);
+
+		contentPane.add(getJButton(), null); // añadimos el boton en la interfaz
 	}
+
+	// ------------- botón de registro------------------------------------
+
+	private JButton getJButton() {
+
+		btnRegistro = new JButton("Registrarse");
+		btnRegistro.setBounds(205, 253, 107, 25);
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				try {
+
+					ApplicationFacadeInterfaceWS facade = MainGUI.getBusinessLogic();
+					nombre = textNombre.getText();
+					usuario = textUsuario.getText();
+					cuenta = textCuenta.getText();
+					pass= passwordField.getText();
+					if (!ComprobarCamposVacios()) {
+						if (ConfirmarDatos()) {
+							if (rdbtnCliente.isSelected()) {
+								Users u = facade.crearCliente(nombre, usuario, pass, cuenta);
+								System.out.println(u.toString() + "registrado como Cliente;");
+							}
+							if (rdbtnOwner.isSelected()) {
+								System.out.println("no te has registrado porque no esta implementado");
+							}
+							siRegistro();
+							dispose();
+						} else {
+							noRegistro();
+							dispose();
+						}
+
+					}
+
+				} catch (OverlappingUsersExists e) { //over
+					usuarioExiste();
+					} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+
+			}
+		});
+
+		return btnRegistro;
+	}
+
+	// --------------------------------------------------------------------
+	// metodos para alertas
 	private boolean ComprobarCamposVacios() {
 		String message = "Porfavor rellene todos los campos";
-		if (cuenta.trim().equals("") || pass.trim().equals("")
-				|| usuario.trim().equals("") || nombre.trim().equals("")) {
-			JOptionPane.showMessageDialog(null, message, "Error",
-					JOptionPane.WARNING_MESSAGE);
+		System.out.println(cuenta.trim());
+		System.out.println(pass.trim());
+		System.out.println(usuario.trim());
+		System.out.println(nombre.trim());
+
+		if (cuenta.trim().equals("") || pass.trim().equals("") || usuario.trim().equals("")
+				|| nombre.trim().equals("")) {
+			JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.WARNING_MESSAGE);
 			return true;
 		} else {
 			return false;
 		}
 
 	}
-//bajada
-	
+
+	private boolean ConfirmarDatos() {
+
+		String nl = System.getProperty("line.separator");
+
+		String message = "Porfavor compruebe que los siguientes datos son correctos:" + nl + "Nombre: " + nombre + nl
+				+ "Nombre de Usuario: " + usuario + nl + "Contraseña: " + pass + nl + "Cuenta Bancaria: " + cuenta;
+
+		int selection = JOptionPane.showConfirmDialog(null, message, "Confirmation", JOptionPane.YES_NO_OPTION);
+
+		return selection == 0;
+	}
+
+	private void siRegistro() {
+
+		String message = "El Usuario " + usuario + " ha sido registrado con exito!";
+		JOptionPane.showMessageDialog(null, message, "Registro Completado", JOptionPane.DEFAULT_OPTION);
+
+		textNombre.setText("");
+		textUsuario.setText("");
+		passwordField.setText("");
+		textCuenta.setText("");
+	}
+
+	private void noRegistro() {
+
+		String message = "El Usuario " + usuario + " no ha sido registrado!";
+		JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+
+		textNombre.setText("");
+		textUsuario.setText("");
+		passwordField.setText("");
+		textCuenta.setText("");
+	}
+	private void usuarioExiste() {
+		String message = "El usuario ya esta registrado";
+		JOptionPane.showMessageDialog(null, message, "Error",
+				JOptionPane.WARNING_MESSAGE);
+		textNombre.setText("");
+		textUsuario.setText("");
+		passwordField.setText("");
+		textCuenta.setText("");
+
+	}
 }
